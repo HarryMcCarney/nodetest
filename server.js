ce25d2ea338acded93a633fac6403770b3c66f41
Harry McCarney <hmccarney@gmail.com>
2013-03-27 16:48:05 +0100

    added some stuff

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 var http = require("http")
-http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
+var sql = require('node-sqlserver');
+var conn_str = "Driver={SQL Server Native Client 11.0};Server=88.198.7.228\descartes,1984;Database=testNode;UID=sa;PWD=MaxStirner.1";
+sql.open(conn_str, function (err, conn) {
+    if(err) {
+        console.log("Error opening the connection!");
+        return;
+    }
+    conn.queryRaw("SELECT 1", function (err, results) {
+        if(err) {
+            console.log("Error running query!");
+            return;
+        }
+        http.createServer(function (req, res) {
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            res.end(err || results);
+        }).listen(process.env.PORT);
     });
-    res.end('Hello World\n');
-}).listen(1337);
+});
--- server.ts
+++ server.ts
@@ -1,8 +1,32 @@
 ///<reference path='node.d.ts'/>
 import http = module("http");
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(1337);
+
+// Query with explicit connection
+var sql = require('node-sqlserver');
+var conn_str = "Driver={SQL Server Native Client 11.0};Server=88.198.7.228\descartes,1984;Database=testNode;UID=sa;PWD=MaxStirner.1";
+
+sql.open(conn_str, function (err, conn) {
+    if (err) {
+        console.log("Error opening the connection!");
+        return;
+    }
+    conn.queryRaw("SELECT 1", function (err, results) {
+        if (err) {
+            console.log("Error running query!");
+            return;
+        }
+        http.createServer(function (req, res) {
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            res.end(err || results);
+        }).listen(process.env.PORT);
+
+        })
+    });
+
+
+
+
 
