bb65b0395735bb3db32e2b0bc456850bda172591
Harry McCarney <hmccarney@gmail.com>
2013-03-27 16:07:38 +0100

    added some stuff

--- HttpServer.js
+++ HttpServer.js
@@ -4,5 +4,5 @@ http.createServer(function (req, res) {
         'Content-Type': 'text/plain'
     });
     res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(1337);
+
