1a0f3947b0ae42a708409a11712a01e0761dd674
Harry McCarney <hmccarney@gmail.com>
2013-03-27 16:15:53 +0100

    added some stuff

---
+++ server.ts
@@ -0,0 +1,8 @@
+///<reference path='node.d.ts'/>
+import http = module("http");
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(1337);
+
